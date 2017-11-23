02211a125734091a4cabff3b5ab774a5f8ca1ed8
eswaribala <Parameswaribala@gmail.com>
2017-11-23 17:00:23 +0530

    Add files via upload

---
+++ ExcelFileWriting.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 18 14:11:23 2017
+
+@author: BALASUBRAMANIAM
+"""
+from openpyxl import load_workbook
+import random
+filePath="AnnualReport2017.xlsx"
+
+fileRef= load_workbook(filePath,read_only=False)
+
+sheetNames=fileRef.get_sheet_names()
+print(sheetNames)
+sheet = fileRef.get_sheet_by_name('January_2017')
+for row in range(51,100):
+    for col in range(1,6):
+        sheet.cell(column=col,row=row, 
+                   value="%d" % random.randint(1,10000))
+        
+fileRef.save(filePath)
+
+
+
+
+
+
+
