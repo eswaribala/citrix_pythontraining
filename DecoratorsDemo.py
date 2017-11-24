8cef2bce2f993435bfc86d25d3a2a5d8f7c868e6
eswaribala <Parameswaribala@gmail.com>
2017-11-24 15:18:31 +0530

    Add files via upload

---
+++ DecoratorsDemo.py
@@ -0,0 +1,26 @@
+'''
+Created on 27-Jun-2017
+
+@author: BALASUBRAMANIAM
+'''
+def switch(data):
+    
+    def function_switch(x):
+        print("Switch, " + data.__name__ + " returns:")
+        data(x)
+    return function_switch
+
+def router(data):
+    def function_wrapper(x):
+        print("Router, " + data.__name__ + " returns:")
+        data(x)
+    return function_wrapper
+    
+    
+list=['192.168.17.1','up']
+@switch
+def call(x):
+   print(x)
+
+call(list)
+    
