a36436bf78a13d7d689ea3762e1d169d5248af01
eswaribala <Parameswaribala@gmail.com>
2017-11-24 16:34:20 +0530

    Add files via upload

---
+++ KeyBoardFunctions.py
@@ -0,0 +1,10 @@
+'''
+Created on 21-Aug-2017
+
+@author: BALASUBRAMANIAM
+'''
+import pyautogui
+#caps lock toggle
+pyautogui.press('capslock')
+#open task manager
+pyautogui.hotkey('ctrl','shift','esc')
